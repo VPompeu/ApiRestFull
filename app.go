4e8a1d25512ccb551cafd0faade4522834bfaebf
Victor Pompeu <vd.pompeu@gmail.com>
2024-06-09 20:06:32 -0300

    att

--- app.go
+++ app.go
@@ -27,4 +27,44 @@ func (a *App) Initialize(user, password, dbname string) {
     a.Router = mux.NewRouter()
 }
 
-func (a *App) Run(addr string) { }
+func respondWithError(w http.ResponseWriter, code int, message string) {
+    respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+    response, _ := json.Marshal(payload)
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(code)
+    w.Write(response)
+}
+
+func (a *App) getProduct(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    id, err := strconv.Atoi(vars["id"])
+    if err != nil {
+        respondWithError(w, http.StatusBadRequest, "Invalid product ID")
+        return
+    }
+
+    p := product{ID: id}
+    if err := p.getProduct(a.DB); err != nil {
+        switch err {
+        case sql.ErrNoRows:
+            respondWithError(w, http.StatusNotFound, "Product not found")
+        default:
+            respondWithError(w, http.StatusInternalServerError, err.Error())
+        }
+        return
+    }
+
+    respondWithJSON(w, http.StatusOK, p)
+}
+
+func (a *App) initializeRoutes() {
+    a.Router.HandleFunc("/products", a.getProducts).Methods("GET")
+}
+
+func (a *App) Run(addr string) {
+    log.Fatal(http.ListenAndServe(addr, a.Router))
+}
